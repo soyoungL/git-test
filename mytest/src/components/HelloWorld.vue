2e7de88920b666671b03edf1dd6cc87d11b8e6aa
liushaoyang <liushaoyang@ccclubs.com>
2020-03-30 11:39:22 +0800

    init program

---
+++ mytest/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <div>{{ msg }}</div>
+    <Childs></Childs>
+  </div>
+</template>
+
+<script>
+import Childs from "./child";
+export default {
+  name: 'HelloWorld',
+  components:{
+    Childs
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  font-size: 30px;
+}
+.asd{
+  border: 1px solid #000;
+}
+</style>
---
+++ mytest/src/components/child.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="red asd">6666666666</div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .red{
+        color: red;
+    }
+</style>
